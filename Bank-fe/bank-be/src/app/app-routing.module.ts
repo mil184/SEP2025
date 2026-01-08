539d78b00e82257a0731bc7a48f228bae23b5f0e
mil184 <45104412+mil184@users.noreply.github.com>
2026-01-08 15:37:46 +0100

    added payment card input on fe

--- Bank-fe/bank-be/src/app/app-routing.module.ts
+++ Bank-fe/bank-be/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaymentsComponent } from './payment/payments/payments.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "payment", component: PaymentsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
